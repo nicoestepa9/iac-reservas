0090aac3e3db30fb8d6778caabf0bfb6a108affa
agusmel <agusmel03@live.com.ar>
2025-10-30 14:39:06 -0300

    terraform application

--- main.tf
+++ main.tf
@@ -15,16 +15,63 @@ resource "railway_service" "api" {
   project_id = var.project_id
   name       = "api"
   source_repo= "BackendOrganization/ReservasAirBack"
+  source_repo_branch="main"
 }
 
 resource "railway_service" "front" {
   project_id = var.project_id
   name       = "front"
   source_repo="IgnaBilli/ReservasAirFront"
+  source_repo_branch="main"
 }
 
 /*resource "railway_service" "db" {
   project_id = var.project_id
   name = "mysql-db"
   template = "mysql"
+  source_repo_branch="main"
 }*/
+
+
+resource "railway_variable_collection" "api_env" {
+  service_id = railway_service.api.id
+  environment_id = var.environment_id
+
+  variables = [
+    {
+      name = "BASE_URL"   
+      value = var.BASE_URL
+    },
+    {
+      name = "MYSQL_DATABASE" 
+      value = var.MYSQL_DATABASE
+    },
+    {
+      name = "MYSQLHOST"     
+      value = var.MYSQLHOST
+    },
+    {
+      name = "MYSQLPASSWORD"  
+      value = var.MYSQLPASSWORD
+    },
+    {
+      name = "MYSQLPORT"     
+      value = var.MYSQLPORT
+    },
+    {
+      name = "MYSQLUSER"     
+      value = var.MYSQLUSER
+    },
+    {
+      name = "NODE_ENV"      
+      value = var.NODE_ENV
+    },
+    {
+      name = "PORT"          
+      value = var.PORT
+    },
+    {
+      name = "RABBITMQ_URL"  
+      value = var.RABBITMQ_URL
+  }]
+}
--- variables.tf
+++ variables.tf
@@ -11,5 +11,60 @@ variable "workspace_id" {
 
 variable "project_id" {
   description = "ID del proyecto en Railway"
-  type = string
+  type        = string
+}
+
+variable "environment_id" {
+  description = ""
+  type        = string
+  sensitive   = true
+
+}
+
+# Variables de entorno para el servicio API
+variable "BASE_URL" {
+  description = "URL base de la API"
+  type        = string
+}
+
+variable "MYSQL_DATABASE" {
+  description = "Nombre de la base de datos MySQL"
+  type        = string
+}
+
+variable "MYSQLHOST" {
+  description = "Host del servidor MySQL"
+  type        = string
+}
+
+variable "MYSQLPASSWORD" {
+  description = "Contraseña de la base de datos MySQL"
+  type        = string
+  sensitive   = true
+}
+
+variable "MYSQLPORT" {
+  description = "Puerto de conexión a MySQL"
+  type        = string
+}
+
+variable "MYSQLUSER" {
+  description = "Usuario de la base de datos MySQL"
+  type        = string
+}
+
+variable "NODE_ENV" {
+  description = "Entorno de ejecución de Node.js"
+  type        = string
+}
+
+variable "PORT" {
+  description = "Puerto donde corre la aplicación"
+  type        = string
+}
+
+variable "RABBITMQ_URL" {
+  description = "URL de conexión a RabbitMQ"
+  type        = string
+  sensitive   = true
 }
